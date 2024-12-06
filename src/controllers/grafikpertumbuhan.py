20d170d3b27ab59a93418e1eb5476bc735ba4d25
Michael Alexander Angkawijaya <19623157@std.stei.itb.ac.id>
2024-12-07 01:51:30 +0700

    feat: fetch for grafik

--- src/controllers/grafikpertumbuhan.py
+++ src/controllers/grafikpertumbuhan.py
@@ -1,5 +1,5 @@
 import sqlite3
-from tanaman import Tanaman
+from src.controllers.tanaman import Tanaman
 
 class GrafikPertumbuhan:
     def __init__(self):
@@ -12,8 +12,14 @@ class GrafikPertumbuhan:
         cursor.execute("SELECT tanggal_catatan, tinggi_tanaman FROM dataPertumbuhanTanaman WHERE jenis_tanaman = ? AND index_tanaman = ?", (tanaman.get_jenis(), tanaman.get_index()))
         conn.commit()
         x = cursor.fetchall()
+        tinggi_tanaman = []
+        for tinggi in x:
+            tinggi_tanaman.append(tinggi[1])
+        tanggal_catatan = []
+        for tanggal in x:
+            tanggal_catatan.append(tanggal[0])
         conn.close()
-        return x
+        return tinggi_tanaman, tanggal_catatan
 
 # graph = GrafikPertumbuhan()
 # x = graph.tinggi_terhadap_waktu(Tanaman(jenis_tanaman="JERUK", index_tanaman=2, data_informasi_tanaman=None, data_pertumbuhan_tanaman=None, data_jadwal_perawatan=None))
