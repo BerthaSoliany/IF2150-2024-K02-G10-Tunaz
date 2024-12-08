0dfd19fc6d55b3680d57c7190d8fd18fd3ce6a2d
Michael Alexander Angkawijaya <19623157@std.stei.itb.ac.id>
2024-12-08 14:21:24 +0700

    feat: connect addformentri and adddatapertumbuhan

--- src/components/graphAddFormEntry.py
+++ src/components/graphAddFormEntry.py
@@ -1,12 +1,24 @@
 import flet as ft
 from src.components.button1 import create_button1
-
+from src.controllers.datapertumbuhantanaman import DataPertumbuhanTanaman
+from src.controllers.datapertumbuhantanamancontroller import DataPertumbuhanTanamanController
 
 def graph_add_form_entry_page(page: ft.Page, go_back):
     page.horizontal_alignment = ft.alignment.center
     page.vertical_alignment = ft.alignment.center
     page.bgcolor = "white"
     page.theme = ft.Theme(font_family="Kantumruy-Regular")
+    
+    tanggal_perawatan_field = ft.CupertinoTextField(bgcolor="white", placeholder_text="Masukkan tanggal pertumbuhan di sini... (DD/MM/YYYY)", placeholder_style=ft.TextStyle(color=ft.Colors.GREY_400))
+    tinggi_tanaman_field = ft.CupertinoTextField(bgcolor="white", placeholder_text="Masukkan tinggi tanaman di sini... (dalam cm)", placeholder_style=ft.TextStyle(color=ft.Colors.GREY_400))
+    status_tanaman_dropdown = ft.Dropdown(bgcolor="white", width=126, hint_text="Status", hint_style=ft.TextStyle(color="black"), border_width=0, text_style=ft.TextStyle(color="black"), options=[ft.dropdown.Option("Hidup"), ft.dropdown.Option("Mati")])
+    kondisi_daun_field = ft.CupertinoTextField(bgcolor="white", placeholder_text="Masukkan kondisi daun di sini...", placeholder_style=ft.TextStyle(color=ft.Colors.GREY_400))
+    def on_click_add(e):
+        data_pertumbuhan_controller = DataPertumbuhanTanamanController()
+        data_pertumbuhan = DataPertumbuhanTanaman(status_tanaman_dropdown.value, tinggi_tanaman_field.value, tanggal_perawatan_field.value, kondisi_daun_field.value)
+        data_pertumbuhan_controller.tambah_data_pertumbuhan("JERUK", 2, data_pertumbuhan) # nanti jeruk, 2 nya diganti sesuai tanamannya
+        go_back(e)
+
     form_card = ft.Card(
         content=ft.Container(
             content=ft.Column(
@@ -31,19 +43,19 @@ def graph_add_form_entry_page(page: ft.Page, go_back):
                         alignment="spaceBetween",
                     ),
                     ft.Text("Tanggal Pertumbuhan", size=20, color="black"),
-                    ft.CupertinoTextField(bgcolor="white", placeholder_text="Masukkan tanggal pertumbuhan di sini... (DD/MM/YYYY)", placeholder_style=ft.TextStyle(color=ft.Colors.GREY_400)), 
+                    tanggal_perawatan_field,
                     ft.Row(
                         [
                             ft.Column([
                                 ft.Text("Tinggi Tanaman", size=20, color="black"),
-                                ft.CupertinoTextField(bgcolor="white", placeholder_text="Masukkan tinggi tanaman di sini... (dalam cm)", placeholder_style=ft.TextStyle(color=ft.Colors.GREY_400)), 
+                                tinggi_tanaman_field, 
                             ],
                             width=586,
                             alignment=ft.MainAxisAlignment.START,
                             horizontal_alignment=ft.CrossAxisAlignment.START,),
                             ft.Column([
                                 ft.Text("Status Tanaman", size=20, color="black"),
-                                ft.Dropdown(bgcolor="white", width=126, hint_text="Status", hint_style=ft.TextStyle(color="black"), border_width=0, text_style=ft.TextStyle(color="black"), options=[ft.dropdown.Option("Hidup"), ft.dropdown.Option("Mati")]), 
+                                status_tanaman_dropdown,
                             ],
                             alignment=ft.MainAxisAlignment.START,
                             horizontal_alignment=ft.CrossAxisAlignment.START,),  
@@ -52,10 +64,10 @@ def graph_add_form_entry_page(page: ft.Page, go_back):
                         vertical_alignment=ft.CrossAxisAlignment.START,
                     ),
                     ft.Text("Kondisi Daun", size=20, color="black"),
-                    ft.CupertinoTextField(bgcolor="white", placeholder_text="Masukkan kondisi daun di sini...", placeholder_style=ft.TextStyle(color=ft.Colors.GREY_400)),
+                    kondisi_daun_field, 
                     ft.Row(
                         controls=[
-                        ft.OutlinedButton(text="TAMBAH", on_click=go_back, width=142, style=ft.ButtonStyle(color="#5F9356", shape=ft.RoundedRectangleBorder(radius=10), side=ft.BorderSide(color="#5F9356", width=2))),
+                        ft.OutlinedButton(text="TAMBAH", on_click=on_click_add, width=142, style=ft.ButtonStyle(color="#5F9356", shape=ft.RoundedRectangleBorder(radius=10), side=ft.BorderSide(color="#5F9356", width=2))),
                         ft.OutlinedButton(text="BATAL", on_click=go_back, width=142, style=ft.ButtonStyle(color="#F47A6F", shape=ft.RoundedRectangleBorder(radius=10), side=ft.BorderSide(color="#F47A6F", width=2))),
                         ], 
                         alignment=ft.MainAxisAlignment.END),
