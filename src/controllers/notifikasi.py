c8de46ac26e9478ea3dc9412b4a4d8cfab90383a
unknown <19623235@std.stei.itb.ac.id>
2024-12-13 17:50:16 +0700

    fix: path to second version icon Tunaz

--- src/controllers/notifikasi.py
+++ src/controllers/notifikasi.py
@@ -1,10 +1,13 @@
 import datetime
 import time
 import locale
+import os
 from winotify import Notification, audio
 
 def notification(target_datetime, title, duration="long"):
-    locale.setlocale(locale.LC_TIME, 'id_ID') #menggunakan bahasa indonesia
+    icon_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../img/logo_tunaz2.png'))
+    locale.setlocale(locale.LC_TIME, 'id_ID')  # menggunakan bahasa Indonesia
+    
     while True:
         current_datetime = datetime.datetime.now()
         current_time_str = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
@@ -17,14 +20,15 @@ def notification(target_datetime, title, duration="long"):
                 title=title,
                 msg=notification_message,
                 duration=duration,
-                icon="./img/logo_tunaz2.png"
+                icon=icon_path,
             )
-            notif.set_audio(audio.Reminder, loop=True)
+            notif.set_audio(audio.Reminder, loop=True)  # Pilihan Audio: Default, Mail, NewMail, SMS, Reminder
             notif.show()
             break
-        time.sleep(1)  # Sleep for 1 second before checking again
+        time.sleep(1)  # Tidur selama 1 detik sebelum memeriksa lagi
 
 
-target_datetime = "2024-12-12 00:05:00" #format 'YYYY-MM-DD HH:MM:SS'
+# Contoh penggunaan:
+target_datetime = "2024-12-12 15:43:00"  # format 'YYYY-MM-DD HH:MM:SS'
 notification_title = "Siram Jagung 001"
 notification(target_datetime, notification_title)
